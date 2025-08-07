83eff8365c45104fd7422b17a3d7ffddc84f4af4
Kaleb Coberly <kalebcoberly@gmail.com>
2025-08-06 18:31:54 -0700

    Add comment.

--- src/stormwater_monitoring_datasheet_extraction/lib/schema/checks/__init__.py
+++ src/stormwater_monitoring_datasheet_extraction/lib/schema/checks/__init__.py
@@ -10,3 +10,10 @@ from stormwater_monitoring_datasheet_extraction.lib.schema.checks.dataframe_chec
 # This would allow us to generate better error messages, and would be more flexible
 # for future changes.
 # Consider doing this.
+# On the other hand, would also be vulnerable to changes in nullability. If we find out a
+# field can be null, and we've made a custom class to carry out our validations, we'd need
+# to then coerce the null into the class, which in itself could be tricky, and which would
+# also mean we'd end up with a non-null value, even if empty. (In other words, we'd get too
+# "Pythonic" for real data integrity.)
+# Also, we'd end up having field validations in multiple places unless we chose one approach
+# or the other.
