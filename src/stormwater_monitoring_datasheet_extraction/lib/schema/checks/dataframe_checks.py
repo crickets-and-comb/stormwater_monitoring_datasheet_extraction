9867b08b85b5220a138433e0f636943edbed1831
Kaleb Coberly <kalebcoberly@gmail.com>
2025-08-08 16:40:22 -0700

    Use better PK check logic.

--- src/stormwater_monitoring_datasheet_extraction/lib/schema/checks/dataframe_checks.py
+++ src/stormwater_monitoring_datasheet_extraction/lib/schema/checks/dataframe_checks.py
@@ -8,4 +8,4 @@ from pandera import extensions
 @extensions.register_check_method(statistics=["pk_cols"])
 def pk_check(df: pd.DataFrame, pk_cols: list[str]) -> bool:
     """Assert that a DataFrame is unique on the primary key columns."""
-    return df[pk_cols].drop_duplicates().shape[0] == df.shape[0]
+    return not df[pk_cols].duplicated().any()
