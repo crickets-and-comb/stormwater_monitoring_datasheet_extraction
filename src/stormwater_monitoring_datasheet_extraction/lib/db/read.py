475302d67503ae1f6759f7133b5adb5a56dfdd8c
Kaleb Coberly <kalebcoberly@gmail.com>
2025-09-06 22:51:59 -0700

    Enquote cast type in read utilities stubs. Error persists.

--- src/stormwater_monitoring_datasheet_extraction/lib/db/read.py
+++ src/stormwater_monitoring_datasheet_extraction/lib/db/read.py
@@ -14,7 +14,7 @@ from stormwater_monitoring_datasheet_extraction.lib import constants, schema
 def get_site_type_map() -> pt.DataFrame[schema.Site]:
     """Reads in the site type map."""
     site_type_map = cast(
-        pt.DataFrame[schema.Site],
+        "pt.DataFrame[schema.Site]",
         pd.DataFrame(
             columns=[constants.Columns.SITE_ID, constants.Columns.OUTFALL_TYPE],
             data=[],
@@ -29,7 +29,7 @@ def get_site_type_map() -> pt.DataFrame[schema.Site]:
 def get_creek_type_map() -> pt.DataFrame[schema.Creek]:
     """Reads in the creek type map."""
     creek_type_map = cast(
-        pt.DataFrame[schema.Creek],
+        "pt.DataFrame[schema.Creek]",
         pd.DataFrame(
             columns=[constants.Columns.SITE_ID, constants.Columns.CREEK_TYPE],
             data=[],
